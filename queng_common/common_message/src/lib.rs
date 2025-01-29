49981136d218838c5e9b799fd73c77a7e48c7f17
Marvin Hansen <marvin.hansen@gmail.com>
2025-01-29 18:07:55 +0800

    Removed unused common message crate
     Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>
    
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_common/common_message/src/lib.rs
+++
@@ -1,7 +0,0 @@
-mod ims_stream_user;
-mod message_client_channel;
-mod message_client_config;
-
-pub use crate::ims_stream_user::*;
-pub use crate::message_client_channel::*;
-pub use crate::message_client_config::*;
--- queng_common/common_message/tests/mod.rs
+++
@@ -1,6 +0,0 @@
-#[cfg(test)]
-mod ims_stream_user_tests;
-#[cfg(test)]
-mod message_client_channel_tests;
-#[cfg(test)]
-mod message_client_config_tests;
