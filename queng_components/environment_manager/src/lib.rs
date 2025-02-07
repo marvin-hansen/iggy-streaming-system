bfc2579358bfee704783e409f637ed690fff7d24
Marvin Hansen <marvin.hansen@gmail.com>
2025-02-07 14:10:28 +0800

    Updated environment_manager
     Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>
    
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_components/environment_manager/src/lib.rs
+++ queng_components/environment_manager/src/lib.rs
@@ -1,4 +1,5 @@
 mod util;
+mod api;
 
 use common_env::EnvironmentType;
 use common_platform::PlatformType;
@@ -41,33 +42,5 @@ impl EnvironmentManager {
             env_type,
             platform_type,
         }
-    }
-}
-
-impl EnvironmentManager {
-    /// Returns the type of environment.
-    ///
-    /// This method returns the `EnvironmentType` of the current `EnvironmentManager` instance.
-    ///
-    /// # Returns
-    ///
-    /// The `EnvironmentType` associated with this manager.
-    ///
-    #[inline]
-    pub const fn env_type(&self) -> EnvironmentType {
-        self.env_type
-    }
-
-    /// Returns the platform type.
-    ///
-    /// This method returns the `PlatformType` of the current `EnvironmentManager` instance.
-    ///
-    /// # Returns
-    ///
-    /// The `PlatformType` associated with this manager.
-    ///
-    #[inline]
-    pub const fn platform_type(&self) -> PlatformType {
-        self.platform_type
     }
 }
