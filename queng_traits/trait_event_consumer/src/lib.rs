f6e0c48fa7fcaba8b7dc73a851d8a15392016608
Marvin Hansen <marvin.hansen@gmail.com>
2025-02-07 13:07:38 +0800

    Removed unused traits.
     Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>
    
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_traits/trait_event_consumer/src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod event_consumer;
-mod event_consumer_error;
-
-pub use event_consumer::*;
-pub use event_consumer_error::*;
--- queng_traits/trait_event_processor/src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod event_processor;
-mod event_processor_error;
-
-pub use event_processor::EventProcessor;
-pub use event_processor_error::EventProcessorError;
